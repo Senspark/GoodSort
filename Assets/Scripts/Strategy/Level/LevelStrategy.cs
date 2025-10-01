df9ba00ed45ddb14b94a45d4b4f3b9ee0502448f
KhoaTr-n <engineer03@senspark.com>
2025-10-01 19:12:18 +0700

    Generate object v1

--- Assets/Scripts/Strategy/Level/LevelStrategy.cs
+++ Assets/Scripts/Strategy/Level/LevelStrategy.cs
@@ -11,10 +11,10 @@ namespace Strategy.Level
         public int NormalBox;
         public int NormalLockedBox;
         public int NormalBoxLayer;
-        public int RowBox;
+        public int RowRange;
         public int RowLockedBox;
         public int RowBoxLayer;
-        public int ColumnBox;
+        public int ColumnRange;
         public int ColumnLockedBox;
         public int ColumnBoxLayer;
         public int SpecialBox;
@@ -23,8 +23,8 @@ namespace Strategy.Level
         public float Density;
 
         [JsonConstructor]
-        public LevelStrategy(int id, int timeLimit, int normalBox, int normalLockedBox, int normalBoxLayer, int rowBox,
-            int rowLockedBox, int rowBoxLayer, int columnBox, int columnLockedBox, int columnBoxLayer, int specialBox,
+        public LevelStrategy(int id, int timeLimit, int normalBox, int normalLockedBox, int normalBoxLayer, int rowRange,
+            int rowLockedBox, int rowBoxLayer, int columnRange, int columnLockedBox, int columnBoxLayer, int specialBox,
             int specialBoxLayer, int group, float density)
         {
             Id = id;
@@ -32,10 +32,10 @@ namespace Strategy.Level
             NormalBox = normalBox;
             NormalLockedBox = normalLockedBox;
             NormalBoxLayer = normalBoxLayer;
-            RowBox = rowBox;
+            RowRange = rowRange;
             RowLockedBox = rowLockedBox;
             RowBoxLayer = rowBoxLayer;
-            ColumnBox = columnBox;
+            ColumnRange = columnRange;
             ColumnLockedBox = columnLockedBox;
             ColumnBoxLayer = columnBoxLayer;
             SpecialBox = specialBox;
--- Assets/Scripts/Utilities/AutoGenerateLevel.cs
+++ Assets/Scripts/Utilities/AutoGenerateLevel.cs
@@ -1,3 +1,4 @@
+using Defines;
 using UnityEngine;
 
 namespace Utilities
@@ -8,8 +9,64 @@ namespace Utilities
         [Header("Prefab")] 
         [SerializeField] private GameObject prefabNormalBox;
         [SerializeField] private GameObject prefabNormalLockedBox;
-        [SerializeField] private GameObject prefabRowBox;
-        [SerializeField] private GameObject prefabColumnBox;
+        [SerializeField] private GameObject prefabRowRange;
+        [SerializeField] private GameObject prefabColumnRange;
         [SerializeField] private GameObject prefabSpecialBox;
+
+        private LevelConfig configs;
+
+        private void LoadConfigs()
+        {
+            if (configs != null) return;
+            var jsonFile = Resources.Load<TextAsset>("Config/level_config");
+            configs = JsonUtility.FromJson<LevelConfig>(jsonFile.text);
+        }
+        
+        public void Generate()
+        {
+            LoadConfigs();
+            var config = System.Array.Find(configs.levelStrategies, x => x.Id == levelID);
+            if (config == null) return;
+            ClearChildren();
+            
+            // Normal box
+            for (var i = 0; i < config.NormalBox - config.NormalLockedBox; i++)
+            {
+                Instantiate(prefabNormalBox, transform);
+            }
+            
+            // Normal locked box
+            for (var i = 0; i < config.NormalLockedBox; i++)
+            {
+                Instantiate(prefabNormalLockedBox, transform);
+            }
+            
+            // Row box
+            for (var i = 0; i < config.RowRange; i++)
+            {
+                Instantiate(prefabRowRange, transform);
+            }
+            
+            // Column box
+            for (var i = 0; i < config.ColumnRange; i++)
+            {
+                Instantiate(prefabColumnRange, transform);
+            }
+            
+            // Special box
+            for (var i = 0; i < config.SpecialBox; i++)
+            {
+                Instantiate(prefabSpecialBox, transform);
+            }
+            Debug.Log($"Generated level {levelID} with {config.NormalBox} normal boxes, {config.NormalLockedBox} locked boxes, {config.RowRange} row boxes, {config.ColumnRange} column boxes, {config.SpecialBox} special boxes");
+        }
+        
+        private void ClearChildren()
+        {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
     }
 }
