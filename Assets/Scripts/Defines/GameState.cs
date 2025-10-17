37c480dc30e3687dd7fbbb891869dc332b33e032
KhoaTr-n <engineer03@senspark.com>
2025-10-17 17:32:49 +0700

    Fix: find nearest slot to auto snap

--- Assets/Scripts/Defines/GameState.cs
+++ Assets/Scripts/Defines/GameState.cs
@@ -6,7 +6,6 @@ namespace Defines
         Initialized, // đã load xong scene
         Loaded, // đã sẵn sàng
         Started, // đã bắt đầu chơi
-        InGame, // đang chơi
         Paused, // đang tạm dừng
         GameOver, // đã kết thúc
         GameConcluded // đã kết thúc và đã show kết quả
--- Assets/Scripts/UI/GameScene.cs
+++ Assets/Scripts/UI/GameScene.cs
@@ -12,7 +12,6 @@ using Senspark;
 using Sirenix.OdinInspector;
 using Strategy.Level;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace UI
 {
@@ -21,14 +20,16 @@ namespace UI
         [SerializeField] private DragDropManager2 dragDropManager;
         [SerializeField] private GameObject container;
         [SerializeField] private ShelfItemBasic shelfItemPrefab;
+        [SerializeField] private LevelUI levelUI;
 
         [CanBeNull] private LevelDataManager _levelDataManager;
         [CanBeNull] private LevelAnimation _levelAnimation;
 
-        private ILevelLoaderManager _levelLoaderManager;
+        private ILevelLoaderManager _levelLoaderManager; 
         private IConfigManager _configManager;
 
         private GameStateType State { get; set; } = GameStateType.UnInitialize;
+        private LevelView _levelView;
 
         private void Awake()
         {
@@ -62,17 +63,30 @@ namespace UI
         private void Start()
         {
             if (State != GameStateType.Initialized) return;
+            Time.timeScale = 1;
             State = GameStateType.Loaded;
             dragDropManager.Init(CanAcceptDropInto);
+            dragDropManager.SetOnDragStarted(OnDragStarted);
             CleanUp();
-            LoadLevel(1);
-            CreateLevel(1);
+            LoadLevel(3);
         }
 
         private void Update()
         {
             var dt = Time.deltaTime;
             _levelAnimation?.Update(dt);
+            ProcessUpdate();
+        }
+
+        private void ProcessUpdate()
+        {
+            if (State == GameStateType.Paused) return;
+            if (State == GameStateType.Started)
+            {
+                if (_levelView)                                                             
+                    _levelView.Step(Time.deltaTime);
+            }
+            
         }
         
         private void LoadLevel(int level)
@@ -80,159 +94,21 @@ namespace UI
             var builder = new LevelConfigBuilder(_levelLoaderManager).SetLevel(level).Build();
             var leveView = builder.LevelObject.GetComponent<LevelView>();
             leveView.transform.SetParent(container.transform,false);
-        }
-
-        public void CreateLevel(int level)
-        {
+            
             var levelConfig = _configManager.GetValue<PuzzleLevelConfig>(ConfigKey.LevelConfig);
             var levelCreator = new LevelCreator(container, shelfItemPrefab);
             var inputData = levelConfig.GetLevel(level).Shelves;
-            // var inputData = new ShelfPuzzleInputData[]
-            // {
-            //     // Shelf 0: [[2], [7,9,9], [11,11,17], [19,33,33]]
-            //     new()
-            //     {
-            //         Type = ShelfType.Common,
-            //         Data = new[]
-            //         {
-            //             new[] { 2, 0, 0 },
-            //             new[] { 7, 9, 9 },
-            //             new[] { 11, 11, 17 },
-            //             new[] { 19, 33, 33 },
-            //         }
-            //     },
-            //
-            //     // Shelf 1: [[8], [8,8,12], [14,16,16], [21,23,23]]
-            //     new()
-            //     {
-            //         Type = ShelfType.Common,
-            //         Data = new[]
-            //         {
-            //             new[] { 8, 0, 0 },
-            //             new[] { 8, 8, 12 },
-            //             new[] { 14, 16, 16 },
-            //             new[] { 21, 23, 23 },
-            //         }
-            //     },
-            //
-            //     // Shelf 2: [[3,3], [4,10,10], [17,31], [24,25,26]]
-            //     new()
-            //     {
-            //         Type = ShelfType.Common,
-            //         Data = new[]
-            //         {
-            //             new[] { 0, 3, 3 },
-            //             new[] { 4, 10, 10 },
-            //             new[] { 0, 17, 31 },
-            //             new[] { 24, 25, 26 },
-            //         }
-            //     },
-            //
-            //     // Shelf 3: [[2,2], [1,9,11], [18,19,19], [16,29,29]]
-            //     new()
-            //     {
-            //         Type = ShelfType.Common,
-            //         Data = new[]
-            //         {
-            //             new[] { 0, 2, 2 },
-            //             new[] { 1, 9, 11 },
-            //             new[] { 18, 19, 19 },
-            //             new[] { 16, 29, 29 },
-            //         }
-            //     },
-            //
-            //     // Shelf 4: [[1,1,5], [7,7], [21,21,22], [25,25,26]]
-            //     new()
-            //     {
-            //         Type = ShelfType.Common,
-            //         Data = new[]
-            //         {
-            //             new[] { 1, 1, 5 },
-            //             new[] { 0, 7, 7 },
-            //             new[] { 21, 21, 22 },
-            //             new[] { 25, 25, 26 },
-            //         }
-            //     },
-            //
-            //     // Shelf 5: [[28,28,30], [13,32], [14,17,20], [15,15,31]]
-            //     new()
-            //     {
-            //         Type = ShelfType.Common,
-            //         Data = new[]
-            //         {
-            //             new[] { 28, 28, 30 },
-            //             new[] { 0, 13, 32 },
-            //             new[] { 14, 17, 20 },
-            //             new[] { 15, 15, 31 },
-            //         }
-            //     },
-            //
-            //     // Shelf 6: [[6,30,30], [3,4,14], [18,18,24], [15,27,24]]
-            //     new()
-            //     {
-            //         Type = ShelfType.Common,
-            //         Data = new[]
-            //         {
-            //             new[] { 6, 30, 30 },
-            //             new[] { 3, 4, 14 },
-            //             new[] { 18, 18, 24 },
-            //             new[] { 15, 27, 24 },
-            //         }
-            //     },
-            //
-            //     // Shelf 7: [[4,6,6], [12,13,13], [20,23,26], [31,32,32]]
-            //     new()
-            //     {
-            //         Type = ShelfType.Common,
-            //         Data = new[]
-            //         {
-            //             new[] { 4, 6, 6 },
-            //             new[] { 12, 13, 13 },
-            //             new[] { 20, 23, 26 },
-            //             new[] { 31, 32, 32 },
-            //         }
-            //     },
-            //
-            //     // Shelf 8: [[5,5,28], [10,12,29], [20,22,22], [27,27,33]]
-            //     new()
-            //     {
-            //         Type = ShelfType.Common,
-            //         Data = new[]
-            //         {
-            //             new[] { 5, 5, 28 },
-            //             new[] { 10, 12, 29 },
-            //             new[] { 20, 22, 22 },
-            //             new[] { 27, 27, 33 },
-            //         }
-            //     },
-            // };
             var levelData = levelCreator.SpawnLevel(inputData, OnItemDestroy);
             _levelDataManager = new LevelDataManager(levelData);
             _levelAnimation = new LevelAnimation(_levelDataManager, dragDropManager);
             _levelAnimation.Enter();
+            
+            leveView.Initialize(levelUI, levelConfig.GetLevel(level));
+            _levelView = leveView;
         }
-
-        [Button]
-        public void SetLevel(int level)
-        {
-            if (State <= GameStateType.Initialized)
-            {
-            }
-            else
-            {
-                LoadLevel(level);
-            }
-        }
-
+        
         private void CleanUp()
         {
-            // if (!_levelView)
-            // {
-            //     return;
-            // }
-            //
-            // Destroy(_levelView.gameObject);
-            // _levelView = null;
             dragDropManager.RemoveAll();
             _levelDataManager?.GetItems().ForEach(e => e?.DestroyItem());
             _levelDataManager?.Dispose();
@@ -258,6 +134,13 @@ namespace UI
         {
             dragDropManager.UnregisterDragObject(itemMeta.Id);
             _levelDataManager?.RemoveItem(itemMeta.Id);
+        }
+        
+        private void OnDragStarted(IDragObject dragObject)
+        {
+            if (State > GameStateType.Started) return;
+            State = GameStateType.Started;
+            // Chuyển state Started khi bắt đầu drag lần đầu tiên
         }
         
         [Button]
